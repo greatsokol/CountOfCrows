7f664a011c1ee6ca671fd82c769ab718c331a577
Administrator <Administrator@BT3-05>
2024-01-29 16:27:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CountOfCrows
+{
+    internal class Program
+    {
+        private static string GetCrowsWordInRightCase(int count)
+        {
+            count = count >= 0 && count <= 20 ? count : count % 10;
+            return count switch
+            {
+                1 => "ворона",
+                2 or 3 or 4 => "вороны",
+                0 or _ => "ворон",
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите число ворон на ветке:");
+                int count = int.Parse(Console.ReadLine());
+                Console.WriteLine($"На ветке {count} {GetCrowsWordInRightCase(count)}");
+            }
+        }
+    }
+}
